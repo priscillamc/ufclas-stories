957144e9798cfdbf05a96aadcf7e100fc94bf06a
priscillamc <priscillamc@email>
2014-05-02 19:57:40 -0400

    Adds links to the header

--- header.php
+++ header.php
@@ -24,9 +24,12 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<div class="container_24">
-                <div class="grid_16"><h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1></div>
+                <div class="grid_16">
+                	<h1 class="site-title"><span><?php echo get_bloginfo('name'); ?></span> <a href="http://oral.history.ufl.edu/"><?php echo get_bloginfo('description'); ?></a></h1>
+                </div>
                 <div class="grid_8">
-                    <h2 class="site-description"><?php bloginfo( 'description' ); ?><br /><?php bloginfo( 'description' ); ?></h2>
+                    <h2 class="site-description"><a href="http://clas.ufl.edu/">College of Liberal Arts and Sciences</a>
+                    <br /><a href="http://ufl.edu/">University of Florida</a></h2>
                 </div>
             </div>
 		</div>
